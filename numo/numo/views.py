87482f02ef475b07df25aded35e45d2cb33019f8
Ih00r <igorknish339@gmail.com>
2024-05-01 19:56:55 +0300

    worked home, reg, log

--- numo/numo/views.py
+++ numo/numo/views.py
@@ -1,31 +1,35 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import login, authenticate
+from django.views import View
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'home.html', {})
-def register(request):
+def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = form.save()
             login(request, user)
-            return redirect('home')  # Змініть 'home' на вашу домашню сторінку
+            return redirect('home')  # Redirect to a different template
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
-def login(request):
+
+def login_view(request):
     if request.method == 'POST':
-        form = CustomAuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
             login(request, user)
-            return redirect('home')  # Змініть 'home' на вашу домашню сторінку
+            return redirect('home')
+        else:
+            messages.error(request, 'Неправильне ім\'я користувача або пароль')
+            return redirect('/registration')
     else:
-        form = CustomAuthenticationForm()
-    return render(request, 'registration/login.html', {'form': form})
+        return render(request, 'registration/login.html')
